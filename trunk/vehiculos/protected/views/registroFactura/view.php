51a8797c3e0a045b92714a2eaa683df1f0fceb74
pancholacho <pancholacho@27ff6e5c-418b-1823-8c12-d3c8355a3dff>
2012-01-30 20:03:42 +0000

    se cambiaron algunos colores y textos de al vista de facturas, ademas se añadio metodo get para obtener el nro de factura al hacer click en agregar orden de trabajonota: se borraron assets para que los cree denuevo

--- trunk/vehiculos/protected/views/registroFactura/view.php
+++ trunk/vehiculos/protected/views/registroFactura/view.php
@@ -6,11 +6,11 @@ $this->breadcrumbs = array(
 );
 
 $this->menu=array(
+        array('label'=>Yii::t('app', 'Agregar') . ' ' . $model->getRelationLabel('ordenTrabajos', 1), 'url'=>array('ordentrabajo/create?factura='.$model->nro_factura.'&id='.$model->id)),
 	array('label'=>Yii::t('app', 'Listar') . ' ' . $model->label(2), 'url'=>array('admin')),
 	array('label'=>Yii::t('app', 'Agregar') . ' ' . $model->label(), 'url'=>array('create')),
 	array('label'=>Yii::t('app', 'Editar') . ' ' . $model->label(), 'url'=>array('update', 'id' => $model->id)),
 	array('label'=>Yii::t('app', 'Borrar') . ' ' . $model->label(), 'url'=>'#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm'=>'Are you sure you want to delete this item?')),
-        array('label'=>Yii::t('app', 'Agregar') . ' ' . $model->getRelationLabel('ordenTrabajos'), 'url'=>array('ordentrabajo/create')),
 );
 ?>
 
@@ -40,12 +40,15 @@ $this->widget('zii.widgets.grid.CGridView', array(
             'header'=>'Patente',
             'value' => 'OrdenTrabajo::formatearPatente($data->idVehiculo->patente)',
         ),
-        //'idVehiculo.patente',
         'fecha',
-        'kilometraje',
+        array(
+                'header'=>'Kilometraje',
+                'value' => 'RegistroFactura::formatearKm($data->kilometraje)',
+            ),
         array(
             'header'=>'Subtotal',
             'value' => 'Vehiculos::formatearPeso($data->sumita)',
+            'htmlOptions'=>array('style' => 'text-align: right;'),
         ),
         array(
             'class' => 'CButtonColumn',
@@ -76,7 +79,7 @@ $this->widget('zii.widgets.grid.CGridView', array(
                 'value' => Vehiculos::formatearPeso($model->total_neto),
             ),
             array(
-                'name'=>'Total Neto',
+                'name'=>'Total Bruto',
                 'value' => Vehiculos::formatearPeso($model->total_bruto),
             ),
         ),

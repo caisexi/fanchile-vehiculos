544d9bc6a419f9cddc156e533c09f809c8b5be4b
pancholacho <pancholacho@27ff6e5c-418b-1823-8c12-d3c8355a3dff>
2012-02-10 21:31:36 +0000

    se formatea salida de peso en grafico

--- trunk/vehiculos/protected/views/site/progresoGasto.php
+++ trunk/vehiculos/protected/views/site/progresoGasto.php
@@ -1,8 +1,13 @@
+<div chart>
 <?php
+
 $data = $dataProvider->getData();
 $this->Widget('ext.highcharts.HighchartsWidget', array(
    'options'=>array(
       'title' => array('text' => 'Progreso Gastos Reparaciones'),
+      'tooltip' => array(
+        'formatter' => 'js:function(){ return "<b>Gastos de "+ this.x +":</b>"+ formatCurrency(this.y); }'
+      ),
       'xAxis' => array(
          'categories' => array('Enero', 'Febrero')
       ),
@@ -18,3 +23,27 @@ $this->Widget('ext.highcharts.HighchartsWidget', array(
    )
 )); 
 ?>
+</div>
+
+<script type="text/javascript">
+    function formatCurrency(num) 
+{
+num = num.toString().replace(/\$|\,/g,'');
+
+if (isNaN(num))
+num = 0;
+
+var signo = (num == (num = Math.abs(num)));
+num = Math.floor(num * 100 + 0.50000000001);
+centavos = num % 100;
+num = Math.floor(num / 100).toString();
+
+if (centavos < 10)
+centavos = '0' + centavos;
+
+for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
+num = num.substring(0, num.length - (4 * i + 3)) + '.' + num.substring(num.length - (4 * i + 3));
+
+return (((signo) ? '' : '-') + '$' + num + ',' + centavos);
+}
+</script>

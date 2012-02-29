9d1404f27f2debb6f1dd69962863b37f9a7dd1b3
pancholacho <pancholacho@27ff6e5c-418b-1823-8c12-d3c8355a3dff>
2012-02-29 14:47:20 +0000

    modelo de bitacoras

---
+++ trunk/vehiculos/protected/models/Bitacoras.php
@@ -0,0 +1,10 @@
+<?php
+
+Yii::import('application.models._base.BaseBitacoras');
+
+class Bitacoras extends BaseBitacoras
+{
+	public static function model($className=__CLASS__) {
+		return parent::model($className);
+	}
+}
